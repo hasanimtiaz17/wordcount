0230ceee09f50e8921cad2ec4714bb2c843d211a
Gloria Lopez <hasanimtiaz17@gmail.com>
2017-01-16 16:38:22 -0600

    Fifth commit

--- mapper.py
+++ mapper.py
@@ -1,10 +1,19 @@
 import sys
-
+import string
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
+    line = line.strip().lower()
+    line = line.translate(None,string.punctuation)
+    line=line.replace("!", "")
+    line=line.replace(",", "") 
+    line=line.replace("?", "")
+    line=line.replace(";", "")
+    line=line.replace("(", "")
+    line=line.replace(")", "")
+    line=line.replace("-", "")    
+
+# split the line into words
     words = line.split()
     # increase counters
     for word in words:
